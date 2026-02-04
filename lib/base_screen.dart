7082d7d98f151bcc1f72ba58a3198ab317d0e60c
Mariana Vieira <108828284+marianavieiras@users.noreply.github.com>
2026-02-04 17:12:49 -0300

    Merge pull request #1 from matsa0/feat/reusable-components
    Feat/reusable components

---
+++ lib/base_screen.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+
+class BaseScreen extends StatefulWidget {
+  const BaseScreen({super.key});
+
+  @override
+  State<BaseScreen> createState() => _BaseScreenState();
+}
+
+class _BaseScreenState extends State<BaseScreen> {
+
+  int _selectedIndex = 0;
+
+  final List<Widget> _screens = const [
+    Center(child: Text("Página inicial"),),
+    Center(child: Text("Buscar por atividade"),),
+    Center(child: Text("Minhas atividades"),),
+    Center(child: Text("Perfil"),)
+  ];
+
+  final List<Widget> _titles = const [
+    Text('Página Inicial', textAlign: TextAlign.left,),
+    Text('Buscar Atividades', textAlign: TextAlign.left,),
+    Text('Minhas Atividades', textAlign: TextAlign.left,),
+    Text('Perfil', textAlign: TextAlign.left,),
+  ];
+
+
+  void _navigateBottomBar(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: _titles[_selectedIndex],
+      ),
+      body: _screens[_selectedIndex],
+
+        bottomNavigationBar: BottomNavigationBar(
+          type: BottomNavigationBarType.fixed,
+          selectedItemColor: Color(0xFFB80D48),
+          currentIndex: _selectedIndex,
+          onTap: _navigateBottomBar, 
+          items: [
+            BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Início'),
+            BottomNavigationBarItem(icon: Icon(Icons.search), label: 'Buscar'),
+            BottomNavigationBarItem(icon: Icon(Icons.bookmark_added_outlined), label: 'Minhas Atividades'),
+            BottomNavigationBarItem(icon: Icon(Icons.person), label: 'Perfil'),
+          ]),
+    );
+  }
+}
---
+++ lib/firebase_options.dart
@@ -0,0 +1,8 @@
+// TODO Implement this library.
+
+import 'package:firebase_core_platform_interface/firebase_core_platform_interface.dart';
+
+class DefaultFirebaseOptions {
+  static FirebaseOptions? get currentPlatform => null;
+  // static const currentPlatform = /* Your FirebaseOptions instance here */;
+}
